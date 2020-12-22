88a0d4f89f3cf81118337675201cbdd58abe2340
HassanKhan <khanhassan057@gmail.com>
2020-12-22 18:57:59 +0500

    stripe backend

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,16 @@
 const Keys = require('../Config/Keys');
-
-
+const Stripe = require('stripe');
+const stripe = Stripe('sk_test_51HzSHtFdvOMEPwYsnZXJISWxcgHhaOeTnWI2HcxAxxwZiJV4QMeJNWCXkEMjKqPypwM2UWaX1Nt04kruEQSedw9k00ctrS4tAA')
 module.exports = app =>{
-    app.use("/api/stripe_handler", (req, res) => {
-      
+    app.use("/api/stripe_handler", async(req, res) => {
+      const charge = await stripe.charges.create({
+          amount: '500',
+          description: "5 surveys for $5",
+          currency: 'usd',
+          source: req.body.id
+      });
+      req.user.credits+=5;
+      const user = await req.user.save();
+      res.send(user);
     })
 }
