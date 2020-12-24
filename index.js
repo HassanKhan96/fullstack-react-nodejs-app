72bfda3b7cbef759002d510eb8f989a4297dde06
HassanKhan <khanhassan057@gmail.com>
2020-12-25 02:15:02 +0500

    error solved

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ mongoose.connect(
 .then(result => console.log('Connected successfully!'))
 .catch(e => console.log("Error: ", e));
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
