ec841851b1d240c01ccb560084f3c273f6889eaa
HassanKhan <khanhassan057@gmail.com>
2020-12-26 02:22:17 +0500

    updated schema

--- models/Surveys.js
+++ models/Surveys.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
     subject: String,
     recepients: [recepientSchema],
     yes: { type: Number, default: 0},
-    no: { type: Number, default: 0}
+    no: { type: Number, default: 0},
+    _user: { type: Schema.Types.ObjectId, ref: 'users'},
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
