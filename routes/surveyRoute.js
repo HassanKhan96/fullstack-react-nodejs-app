208d404380e23c1ba4265b082d38e30256a0ac31
HassanKhan <khanhassan057@gmail.com>
2020-12-27 23:01:05 +0500

    survey route added

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -1,6 +1,17 @@
 const checkAuth = require('../middlewares/checkAuth');
+const checkCredit = require('../middlewares/checkCredit');
+const mongoose = require('mongoose');
+const Survey = mongoose.model('surveys');
 module.exports = app => {
-    app.post('/api/survey', checkAuth,(req, res) => {
-
+    app.post('/api/survey', checkAuth, checkCredit, (req, res) => {
+        const { title, body, subject, recepients } = req.body;
+        const survey = new Survey({
+            title,
+            body,
+            subject,
+            recepients: recepients.split(',').map(email => ({email})),
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
     })
 }
